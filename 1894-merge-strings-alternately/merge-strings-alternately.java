260a607aa9fd22f7aad485ac2b0a1031393c4028
Tanmay Kumar <nikhil270405@gmail.com>
2025-12-17 20:32:56 +0530

    Time: 5 ms (9.40%) | Memory: 43.9 MB (10.38%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,16 +1,16 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String result="";
+        String add="";
         int i=0;
         while(i<word1.length()||i<word2.length()){
             if(i<word1.length()){
-                result=result+word1.charAt(i);
+                add=add+word1.charAt(i);
             }
             if(i<word2.length()){
-                result=result+word2.charAt(i);
+                add=add+word2.charAt(i);
             }
             i++;
         }
-        return result;
+        return add;
     }
 }
