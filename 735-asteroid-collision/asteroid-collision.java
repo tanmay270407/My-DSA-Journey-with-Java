30a72d452dac41c7b4bb6bcc22649b215853e6ab
Tanmay Kumar <nikhil270405@gmail.com>
2026-01-12 20:49:54 +0530

    Time: 1 ms (99.69%) | Memory: 46.6 MB (90.77%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int[] stack = new int[asteroids.length];
+        int top = -1;
+
+        for(int i = 0; i < asteroids.length; i++) {
+            int curr = asteroids[i];
+
+            while(top >= 0 && stack[top] > 0 && curr < 0) {
+                int a = Math.abs(stack[top]);
+                int b = Math.abs(curr);
+
+                if(a > b) {
+                    curr = 0;
+                    break;
+                }
+                else if(a < b) {
+                    top--;
+                }
+                else {
+                    top--;
+                    curr = 0;
+                    break;
+                }
+            }
+
+            if(curr != 0) {
+                stack[++top] = curr;
+            }
+        }
+
+        int[] res = new int[top + 1];
+        for(int i = 0; i <= top; i++) {
+            res[i] = stack[i];
+        }
+        return res;
+    }
+}
