07e6ed53e7140dbb38e980db09f3bbbe6addaa6d
Tanmay Kumar <nikhil270405@gmail.com>
2026-03-24 21:41:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (87.97%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,28 +1,23 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
 
-        int[] dp = new int[n];
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
 
-        // Starting cell
-        dp[0] = (obstacleGrid[0][0] == 0) ? 1 : 0;
+        int[] dp=new int[n];
+    
+        dp[0]=(obstacleGrid[0][0]==0) ? 1 : 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                // If obstacle → no paths
-                if (obstacleGrid[i][j] == 1) {
-                    dp[j] = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[j]=0;
                 } 
-                // Otherwise add from left
-                else if (j > 0) {
-                    dp[j] += dp[j - 1];
+                else if(j>0){
+                    dp[j]+=dp[j-1];
                 }
             }
         }
-
-        return dp[n - 1];
-        
+        return dp[n-1];
     }
 }
