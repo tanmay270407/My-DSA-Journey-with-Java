845377294eeec2c1d26d9ce32e3a714d201de503
Tanmay Kumar <nikhil270405@gmail.com>
2026-04-02 20:22:29 +0530

    Time: 8 ms (76.93%) | Memory: 70.9 MB (81.51%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -12,7 +12,7 @@ class Solution {
                 if (matrix[i][j] == '1') {
 
                     if (i == 0 || j == 0) {
-                        dp[i][j] = 1; // first row/column
+                        dp[i][j] = 1; 
                     } else {
                         dp[i][j] = 1 + Math.min(dp[i-1][j],
                                       Math.min(dp[i][j-1], dp[i-1][j-1]));
@@ -23,6 +23,6 @@ class Solution {
             }
         }
 
-        return maxSide * maxSide; // area
+        return maxSide * maxSide; 
     }
 }
