6204d3d9dbbe810cab87e8fdfb7bcb7bb2465034
Tanmay Kumar <nikhil270405@gmail.com>
2026-04-11 22:34:15 +0530

    Time: 12 ms (36.66%) | Memory: 46.4 MB (61.34%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int calculate(String s) {
+        int result = 0, num = 0, sign = 1;
+        Stack<Integer> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            } else if (c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            } else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * num;
+                num = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+
+        return result + sign * num;
+    }
+}
