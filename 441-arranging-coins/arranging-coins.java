3735581ad5f5fb897c5431539d210ff6c340d907
Tanmay Kumar <nikhil270405@gmail.com>
2026-04-17 23:33:06 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (66.98%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,7 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        return (int)((Math.sqrt(1 + 8L * n) - 1) / 2);
-        
-        
+        return (int)((Math.sqrt(1 + 8L * n)-1)/2);
     }
 }
