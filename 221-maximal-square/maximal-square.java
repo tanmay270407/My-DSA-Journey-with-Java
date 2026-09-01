3c0d1ee502115203187471cb365f4f07ba01a825
Tanmay Kumar <nikhil270405@gmail.com>
2026-09-01 09:57:57 +0530

    Time: 13 ms (9.93%) | Memory: 74.2 MB (12.94%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,28 +1,33 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
+        int ans = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-
-        int[][] dp = new int[m][n];
-        int maxSide = 0;
-
+        Integer[][] dp = new Integer[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
                 if (matrix[i][j] == '1') {
-
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1; 
-                    } else {
-                        dp[i][j] = 1 + Math.min(dp[i-1][j],
-                                      Math.min(dp[i][j-1], dp[i-1][j-1]));
-                    }
-
-                    maxSide = Math.max(maxSide, dp[i][j]);
+                    ans = Math.max(ans, solve(i, j, m, n, matrix, dp));
                 }
             }
         }
+        return ans * ans;
+    }
 
-        return maxSide * maxSide; 
+    public int solve(int i, int j, int m, int n, char[][] matrix, Integer[][] dp) {
+        if (i >= m || j >= n) {
+            return 0;
+        }
+        if (matrix[i][j] == '0') {
+            return dp[i][j] = 0;
+        }
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+        int down = solve(i + 1, j, m, n, matrix, dp);
+        int right = solve(i, j + 1, m, n, matrix, dp);
+        int diagonal = solve(i + 1, j + 1, m, n, matrix, dp);
+        dp[i][j] = 1 + Math.min(down, Math.min(right, diagonal));
+        return dp[i][j];
     }
 }
